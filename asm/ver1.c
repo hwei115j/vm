f28a5c634b9b45e9f2eb3b726e96152eb4e4a17e
hwei115j <hwei115j@gmail.com>
2018-04-12 21:46:09 +0800

    ver1

--- asm/ver1.c
+++ asm/ver1.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #define MAX 110
 
+char *func(char *str, char *code)
+{
+    static s[MAX];
+
+    if(NULL == strstr(str, code))
+        return NULL;
+
+
+}
 int main()
 {
     FILE *input;
     FILE *output;
     char str[MAX];
+    char 
+    int pc = 0;
 
-    input = fopen("./s1", "r");
-    output = fopen("./rom", "w");
+    input = fopen("s1", "r");
+    output = fopen("rom", "rw");
     while(fgets(str, MAX-10, input) != NULL)
     {
         int i;
@@ -17,6 +29,10 @@ int main()
             if(str[i] == '/' && str[i+1] == '/' || str[i] == '\n')
                 str[i] = str[i+1] = '\0';
         fprintf(output, "%s\n", str);
+    }
+    while(fgets(str, MAX-10, output) != NULL)
+    {
+        if(func(str, "ORG") != NULL)
     }
     fclose(output);
 
